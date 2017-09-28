efa0f387362e3ec86daee456e85376f95361589c
Anthony Montuoro <montuoro100@gmail.com>
2017-09-28 17:46:25 -0400

    Changed diff.py filter from [-1, 1, 0] to [-1, 0, 1]

--- proj_1/diff.py
+++ proj_1/diff.py
@@ -4,9 +4,9 @@ import glob
 def diff(pix,thres):
 	result = []
 	for i in range(0,len(pix)):
-		if i == 0:
+		if i == 0 or i == len(pix) - 1:
 			result.append(pix[i])
-		elif abs(pix[i - 1] - pix[i]) > thres:
+		elif abs(pix[i - 1] - pix[i + 1]) > thres:
 			result.append(255)
 		else:
 			result.append(pix[i])
