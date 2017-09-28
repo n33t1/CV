e9a33763a3735388c1aa4d69cc1b678e571016ab
n33t1 <n33t1root@gmail.com>
2017-09-28 19:28:13 -0400

    temp

--- proj_1/main.py
+++ proj_1/main.py
@@ -2,13 +2,13 @@ from PIL import Image
 import numpy as np
 import glob
 from diff import diff
-
+from gaussian import gaussian
 
 image_list = []
 thres = 30 	# Threshold used to determine if something is moving through that pixel
 
 #i. Read in a sequence of image frames and make them grayscale.
-for filename in sorted(glob.glob('RedChair/*.jpg')):
+for filename in sorted(glob.glob('Red/*.jpg')):
     # im = Image.open(filename).convert('LA')
     # im_np = np.asarray(im.getdata())
     # print im_np
@@ -19,34 +19,38 @@ for filename in sorted(glob.glob('RedChair/*.jpg')):
     # print type(im_np), im_np.shape  
     image_list.append(im_np)
     
+print image_list, type(image_list)
 
+# gaussian(image_list)
 
 #Transpose array to be described as a list of lists, where each list is a single pixel's values as it changes over time
-trans_image_list = np.asarray(image_list).T.tolist()
+# trans_image_list = np.asarray(image_list).T.tolist()
+image_array = np.asarray(image_list)
+trans_array = np.transpose(image_array)
 
+image_result = gaussian(trans_array, thres)
 
-#Note: ii and iii will be done at the same time in diff, since its a lot faster to combine both for loops
+# #Note: ii and iii will be done at the same time in diff, since its a lot faster to combine both for loops
 
-#ii. As enough frames are available, apply a 1-D differential operator at each pixel to compute a temporal derivative.
-image_result = []
+# #ii. As enough frames are available, apply a 1-D differential operator at each pixel to compute a temporal derivative.
+# image_result = []
 
-for pix in trans_image_list:
-	im_res = np.asarray(diff(pix,thres), dtype='uint8')
-	image_result.append(im_res)
+# for pix in trans_image_list:
+# 	im_res = np.asarray(diff(pix,thres), dtype='uint8')
+# 	image_result.append(im_res)
 
+# print len(image_result[0])
+# #iii. Threshold the absolute values of the derivatives to create a 0 and 1 mask of the moving objects.
 
-#iii. Threshold the absolute values of the derivatives to create a 0 and 1 mask of the moving objects.
+# #iv. Combine the mask with the original frame to display the results.
 
-#iv. Combine the mask with the original frame to display the results.
-
-# Transpose the results to match the original image
-image_result = np.asarray(image_result).T
-# print image_result.shape
+# # Transpose the results to match the original image
+# image_result = np.asarray(image_result).T
+# # print image_result.shape
 
 
 # output
-
-image_result = np.asarray(image_result, dtype='uint8') #if values still in range 0-255! 
+#image_result = np.asarray(image_result, dtype='uint8') #if values still in range 0-255! 
 i = 0
 for single_image in image_result:
 	out = single_image.reshape((240, 320))
