109c3b81bfbbffee0f65885eb4dc34d68369e6c3
Anthony Montuoro <montuoro100@gmail.com>
2017-10-02 02:23:24 -0400

    First itteration of smoothing box filter

--- proj_1/main.py
+++ proj_1/main.py
@@ -4,14 +4,14 @@ import glob
 from scipy.ndimage.filters import gaussian_filter
 
 image_list = []
-thres = 30 	# Threshold used to determine if something is moving through that pixel
+thres = 50 	# Threshold used to determine if something is moving through that pixel
 
 """function takes np arrary and threshold as input, output a np array classified with only 
 pixel value of 0 or 255"""
 def gaussian(trans_array, thres):
     # prompt user input
     tsigma = input('Enter standard deviation tsigma for Gaussian filter: ')
-    out = gaussian_filter(trans_array, sigma=tsigma)
+    out = gaussian_filter(trans_array, order=1, sigma=tsigma)
     result = []
 
     for time_axis in out:
@@ -49,6 +49,98 @@ def diff(trans_image,thres):
 
 	return result_array
 
+"""function takes np arrary as input, output a np array classified with only 
+pixel value of 0 or 255"""
+def Smooth3x3(input_image):
+	result = []
+	for image in input_image:
+		pix_res = []
+		#iii. Threshold the absolute values of the derivatives to create a 0 and 1 mask of the moving objects.
+		for pix_index in range(0,len(image)):
+			# Top Row
+			if pix_index < 320:
+				# Left Corner
+				if pix_index == 0:
+					pix_res.append(image[pix_index] / 4 + image[pix_index + 1] / 4 + image[pix_index + 320] / 4 + image[pix_index + 321] / 4)
+				# Right Corner
+				elif pix_index == 319:
+					pix_res.append(image[pix_index] / 4 + image[pix_index - 1] / 4 + image[pix_index + 319] / 4 + image[pix_index + 320] / 4)
+				else:
+					pix_res.append(image[pix_index] / 6 + image[pix_index - 1] / 6 + image[pix_index + 1] / 6 + image[pix_index + 319] / 6 + image[pix_index + 320] / 6 + image[pix_index + 321] / 6)
+			# Bottom Row
+			elif pix_index > 76479:
+				# Left Corner
+				if pix_index == 76480:
+					pix_res.append(image[pix_index] / 4 + image[pix_index + 1] / 4 + image[pix_index - 319] / 4 + image[pix_index - 320] / 4)
+				# Right Corner
+				elif pix_index == 76799:
+						pix_res.append(image[pix_index] / 4 + image[pix_index - 1] / 4 + image[pix_index - 320] / 4 + image[pix_index - 321] / 4)
+				else:
+					pix_res.append(image[pix_index] / 6 + image[pix_index - 1] / 6 + image[pix_index + 1] / 6 + image[pix_index - 319] / 6 + image[pix_index - 320] / 6 + image[pix_index - 321] / 6)
+			# Left Column
+			elif pix_index % 320 == 0:
+				pix_res.append(image[pix_index] / 6 + image[pix_index + 1] / 6 + image[pix_index - 319] / 6 + image[pix_index - 320] / 6 + image[pix_index + 320] / 6 + image[pix_index + 321] / 6)
+			# Right Column
+			elif (pix_index + 1) % 320 == 0:
+				pix_res.append(image[pix_index] / 6 + image[pix_index - 1] / 6 + image[pix_index - 320] / 6 + image[pix_index - 321] / 6 + image[pix_index + 319] / 6 + image[pix_index + 320] / 6)
+			# Regular Pixel
+			else:
+				pix_res.append(image[pix_index - 321] / 9 + image[pix_index - 320] / 9 + image[pix_index - 319] / 9 + image[pix_index - 1] / 9 + image[pix_index] / 9 + image[pix_index + 1] / 9 + image[pix_index + 319] / 9 + image[pix_index + 320] / 9 + image[pix_index + 321] / 9)
+
+			#iv. Combine the mask with the original frame to display the results.
+		pix_res_arr = np.asarray(pix_res)
+		result.append(pix_res_arr)
+
+	return np.asarray(result, dtype='uint8')
+
+"""function takes np arrary as input, output a np array classified with only 
+pixel value of 0 or 255"""
+def Smooth3x1(input_image):
+	result = []
+	for image in input_image:
+		pix_res = []
+		#iii. Threshold the absolute values of the derivatives to create a 0 and 1 mask of the moving objects.
+		for pix_index in range(0,len(image)):
+			# Left Column
+			if pix_index % 320 == 0:
+				pix_res.append(image[pix_index] / 2 + image[pix_index + 1] / 2)
+			# Right Column
+			elif (pix_index + 1) % 320 == 0:
+				pix_res.append(image[pix_index] / 2 + image[pix_index - 1] / 2)
+			# Regular Pixel
+			else:
+				pix_res.append(image[pix_index] / 3 + image[pix_index - 1] / 3 + image[pix_index + 1] / 3)
+
+			#iv. Combine the mask with the original frame to display the results.
+		pix_res_arr = np.asarray(pix_res)
+		result.append(pix_res_arr)
+
+	return np.asarray(result, dtype='uint8')
+
+"""function takes np arrary as input, output a np array classified with only 
+pixel value of 0 or 255"""
+def Smooth1x3(input_image):
+	result = []
+	for image in input_image:
+		pix_res = []
+		#iii. Threshold the absolute values of the derivatives to create a 0 and 1 mask of the moving objects.
+		for pix_index in range(0,len(image)):
+			# Top Row
+			if pix_index < 320:
+				pix_res.append(image[pix_index] / 2 + image[pix_index + 320] / 2)
+			# Bottom Row
+			elif pix_index > 76479:
+				pix_res.append(image[pix_index] / 2 + image[pix_index - 320] / 2)
+			# Regular Pixel
+			else:
+				pix_res.append(image[pix_index - 320] / 3 + image[pix_index] / 3 + image[pix_index + 320] / 3)
+
+			#iv. Combine the mask with the original frame to display the results.
+		pix_res_arr = np.asarray(pix_res)
+		result.append(pix_res_arr)
+
+	return np.asarray(result, dtype='uint8')
+
 def output_image(images_output):
     image_result = np.asarray(images_output, dtype='uint8') #if values still in range 0-255! 
     i = 0
@@ -65,8 +157,10 @@ for filename in sorted(glob.glob('RedChair/*.jpg')):
     im_np = np.asarray(im.getdata(), dtype='uint8')
     image_list.append(im_np)
 
+images_output = Smooth1x3(Smooth3x1(image_list))
+
 #Transpose array to be described as a list of lists, where each list is a single pixel's values as it changes over time
-trans_image_list = np.asarray(image_list).T
+trans_image_list = np.asarray(images_output).T
 
 # images_output = gaussian(trans_image_list, thres)
 images_output = diff(trans_image_list, thres)
