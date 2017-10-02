6a97188de06fbf74f49e0baebdbbe23860863ecd
n33t1 <n33t1root@gmail.com>
2017-10-01 21:51:16 -0400

    cleaned up the code

--- proj_1/main.py
+++ proj_1/main.py
@@ -1,59 +1,75 @@
 from PIL import Image
 import numpy as np
 import glob
-from diff import diff
-from gaussian import gaussian
+from scipy.ndimage.filters import gaussian_filter
 
 image_list = []
 thres = 30 	# Threshold used to determine if something is moving through that pixel
 
+"""function takes np arrary and threshold as input, output a np array classified with only 
+pixel value of 0 or 255"""
+def gaussian(trans_array, thres):
+    # prompt user input
+    tsigma = input('Enter standard deviation tsigma for Gaussian filter: ')
+    out = gaussian_filter(trans_array, sigma=tsigma)
+    result = []
+
+    for time_axis in out:
+        pix_res = np.array([255 if pix > thres else pix for pix in time_axis])
+        result.append(pix_res)
+
+    # Transpose the results to match the original image
+    trans_result_array = np.asarray(result, dtype='uint8')
+    result_array = np.transpose(trans_result_array)
+    
+    return result_array
+
+"""function takes np arrary and threshold as input, output a np array classified with only 
+pixel value of 0 or 255"""
+def diff(trans_image,thres):
+    	result = []
+	trans_image_list = trans_image.tolist()
+	for time_axis in trans_image_list:
+		pix_res = []
+		#iii. Threshold the absolute values of the derivatives to create a 0 and 1 mask of the moving objects.
+		for i in range(0,len(time_axis)):
+			if i == 0 or i == len(time_axis) - 1:
+				pix_res.append(time_axis[i])
+			elif abs(time_axis[i - 1] - time_axis[i + 1]) > thres:
+				pix_res.append(255)
+			else:
+				pix_res.append(time_axis[i])
+		#iv. Combine the mask with the original frame to display the results.
+		pix_res_arr = np.asarray(pix_res)
+		result.append(pix_res_arr)
+	
+	# Transpose the results to match the original image
+	trans_result_array = np.asarray(result, dtype='uint8')
+	result_array = np.transpose(trans_result_array)
+
+	return result_array
+
+def output_image(images_output):
+    image_result = np.asarray(images_output, dtype='uint8') #if values still in range 0-255! 
+    i = 0
+    for single_image in image_result:
+        out = single_image.reshape((240, 320))
+        w = Image.fromarray(out, mode='L')
+        w.save('ress/out_%s.jpg' % i)
+        i = i + 1
+    
 #i. Read in a sequence of image frames and make them grayscale.
-for filename in sorted(glob.glob('Red/*.jpg')):
-    # im = Image.open(filename).convert('LA')
-    # im_np = np.asarray(im.getdata())
-    # print im_np
+for filename in sorted(glob.glob('RedChair/*.jpg')):
     im = Image.open(filename)
     im = im.convert('L') #makes it greyscale
-    # y = np.asarray(x.getdata(),dtype=np.float64).reshape((x.size[1],x.size[0]))
     im_np = np.asarray(im.getdata(), dtype='uint8')
-    # print type(im_np), im_np.shape  
     image_list.append(im_np)
-    
-print image_list, type(image_list)
-
-# gaussian(image_list)
 
 #Transpose array to be described as a list of lists, where each list is a single pixel's values as it changes over time
-# trans_image_list = np.asarray(image_list).T.tolist()
-image_array = np.asarray(image_list)
-trans_array = np.transpose(image_array)
+trans_image_list = np.asarray(image_list).T
 
-image_result = gaussian(trans_array, thres)
+# images_output = gaussian(trans_image_list, thres)
+images_output = diff(trans_image_list, thres)
 
-# #Note: ii and iii will be done at the same time in diff, since its a lot faster to combine both for loops
-
-# #ii. As enough frames are available, apply a 1-D differential operator at each pixel to compute a temporal derivative.
-# image_result = []
-
-# for pix in trans_image_list:
-# 	im_res = np.asarray(diff(pix,thres), dtype='uint8')
-# 	image_result.append(im_res)
-
-# print len(image_result[0])
-# #iii. Threshold the absolute values of the derivatives to create a 0 and 1 mask of the moving objects.
-
-# #iv. Combine the mask with the original frame to display the results.
-
-# # Transpose the results to match the original image
-# image_result = np.asarray(image_result).T
-# # print image_result.shape
-
-
-# output
-#image_result = np.asarray(image_result, dtype='uint8') #if values still in range 0-255! 
-i = 0
-for single_image in image_result:
-	out = single_image.reshape((240, 320))
-	w = Image.fromarray(out, mode='L')
-	w.save('out_%s.jpg' % i)
-	i = i + 1
+#output the image
+output_image(images_output)
